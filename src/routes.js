f3e911ba046cf18dbe831d5cdb8fb94fb0f6b2f4
Seba Beltran <seba@trip.com.uy>
2018-06-01 16:52:22 -0600

    Erased unused components

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,6 @@
 import React, {Component} from "react";
 import {HashRouter, Route, Switch} from "react-router-dom"
 import Dashboard from "./components/Dashboard";
-import Wizard_One from "./components/Wizard_One";
-import Wizard_Two from "./components/Wizard_Two";
-import Wizard_Three from "./components/Wizard_Three";
 import Wizard from "./components/Wizard"
 
 export default function(){
@@ -12,9 +9,6 @@ export default function(){
     <Switch>
       <Route component={Dashboard} path="/" exact/>
       <Route component={Wizard} path="/wizard" />
-      {/* <Route component={Wizard_One} path="/wizard/step1"/>
-      <Route component={Wizard_Two} path="/wizard/step2"/>
-      <Route component={Wizard_Three} path="/wizard/step3"/> */}
     </Switch>  
   </HashRouter>  
   )
